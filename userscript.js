baf4352bff0ce1caf984faf2869ce9b1ad0bb66b
Wed0n <wed0n1213@gmail.com>
2024-01-22 09:37:10 +0800

    doc: add userscript license

--- userscript.js
+++ userscript.js
@@ -4,6 +4,7 @@
 // @version      0.1.0
 // @description  驾校教务处登录自动填写验证码
 // @author       Wed0n
+// @license      MIT
 // @match        *.cuit.edu.cn*/authserver/*
 // @icon         https://cdn.jsdelivr.net/gh/wed0n/cuit_captcha@latest/public/vite.svg
 // @grant        none
