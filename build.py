47891e269a5738924aa1af8ab9e84f920f0f899d
Wed0n <wed0n1213@gmail.com>
2024-02-12 16:41:40 +0800

    refactor: change wed0n.cf to wed0n.top

--- build.py
+++ build.py
@@ -24,7 +24,7 @@ with open("package.json") as f:
 
 first = """// ==UserScript==
 // @name         CUIT验证码自动填写
-// @namespace    https://blog.wed0n.cf
+// @namespace    https://blog.wed0n.top
 // @homepage     https://github.com/wed0n/cuit_captcha
 // @version      {}
 // @description  驾校教务处登录自动填写验证码
@@ -38,7 +38,7 @@ first = """// ==UserScript==
 // ==/UserScript==
 
 // 如果你不想忍受Cloudflare在璃月的访问速度较慢，而且你有自己的服务器，你可以修改这个资源路径。
-const resourcePath = "https://static.wed0n.cf/cuit/captcha/";
+const resourcePath = "https://static.wed0n.top/cuit/captcha/";
 // 如果需要自动填写账号密码就修改下方两个变量。
 const username = "";
 const password = "";
